a84c1f982a13497244677ac874d1f275b8b672c6
itsh1no <49691112+itsh1no@users.noreply.github.com>
2020-09-06 13:13:19 +0300

    Add files via upload

---
+++ raid.sql
@@ -0,0 +1,89 @@
+-- --------------------------------------------------------
+-- Sunucu:                       127.0.0.1
+-- Sunucu sürümü:                10.5.3-MariaDB - mariadb.org binary distribution
+-- Sunucu İşletim Sistemi:       Win64
+-- HeidiSQL Sürüm:               11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- shx_rpv için veritabanı yapısı dökülüyor
+CREATE DATABASE IF NOT EXISTS `shx_rpv` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `shx_rpv`;
+
+-- tablo yapısı dökülüyor shx_rpv.character_current
+CREATE TABLE IF NOT EXISTS `character_current` (
+  `cid` varchar(255) NOT NULL,
+  `model` longtext NOT NULL DEFAULT '',
+  `drawables` longtext NOT NULL DEFAULT '',
+  `props` longtext NOT NULL DEFAULT '',
+  `drawtextures` longtext NOT NULL DEFAULT '',
+  `proptextures` longtext NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- shx_rpv.character_current: ~1 rows (yaklaşık) tablosu için veriler indiriliyor
+/*!40000 ALTER TABLE `character_current` DISABLE KEYS */;
+INSERT INTO `character_current` (`cid`, `model`, `drawables`, `props`, `drawtextures`, `proptextures`) VALUES
+	('Char1steam:1100001359ad80f', '1461287021', '{"4":["legs",0],"3":["torsos",0],"2":["hair",0],"1":["masks",0],"0":["face",0],"11":["jackets",0],"10":["decals",0],"9":["vest",0],"8":["undershirts",0],"7":["neck",0],"6":["shoes",0],"5":["bags",0]}', '{"4":["lhand",-1],"3":["mouth",-1],"2":["earrings",-1],"1":["glasses",-1],"0":["hats",-1],"7":["braclets",-1],"6":["watches",-1],"5":["rhand",-1]}', '[["face",0],["masks",0],["hair",0],["torsos",0],["legs",0],["bags",0],["shoes",0],["neck",0],["undershirts",0],["vest",0],["decals",0],["jackets",0]]', '[["hats",-1],["glasses",-1],["earrings",-1],["mouth",-1],["lhand",-1],["rhand",-1],["watches",-1],["braclets",-1]]');
+/*!40000 ALTER TABLE `character_current` ENABLE KEYS */;
+
+-- tablo yapısı dökülüyor shx_rpv.character_face
+CREATE TABLE IF NOT EXISTS `character_face` (
+  `cid` varchar(255) NOT NULL,
+  `hairColor` varchar(255) NOT NULL DEFAULT '',
+  `headBlend` varchar(255) NOT NULL DEFAULT '',
+  `headStructure` varchar(255) NOT NULL DEFAULT '',
+  `headOverlay` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- shx_rpv.character_face: ~0 rows (yaklaşık) tablosu için veriler indiriliyor
+/*!40000 ALTER TABLE `character_face` DISABLE KEYS */;
+INSERT INTO `character_face` (`cid`, `hairColor`, `headBlend`, `headStructure`, `headOverlay`) VALUES
+	('Char1steam:1100001359ad80f', '[-1,-1]', '[]', '[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]', '[]');
+/*!40000 ALTER TABLE `character_face` ENABLE KEYS */;
+
+-- tablo yapısı dökülüyor shx_rpv.character_outfits
+CREATE TABLE IF NOT EXISTS `character_outfits` (
+  `cid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `slot` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `model` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `drawables` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `props` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `drawtextures` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `proptextures` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `hairColor` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- shx_rpv.character_outfits: ~1 rows (yaklaşık) tablosu için veriler indiriliyor
+/*!40000 ALTER TABLE `character_outfits` DISABLE KEYS */;
+INSERT INTO `character_outfits` (`cid`, `name`, `slot`, `model`, `drawables`, `props`, `drawtextures`, `proptextures`, `hairColor`) VALUES
+	('steam:1100001359ad80f', 'sa', '1', '1461287021', '{"1":["masks",0],"2":["hair",0],"3":["torsos",0],"4":["legs",0],"5":["bags",0],"6":["shoes",0],"7":["neck",0],"8":["undershirts",0],"9":["vest",0],"10":["decals",0],"11":["jackets",0],"0":["face",0]}', '{"1":["glasses",-1],"2":["earrings",-1],"3":["mouth",-1],"4":["lhand",-1],"5":["rhand",-1],"6":["watches",-1],"7":["braclets",-1],"0":["hats",-1]}', '[["face",0],["masks",0],["hair",0],["torsos",0],["legs",0],["bags",0],["shoes",0],["neck",0],["undershirts",0],["vest",0],["decals",0],["jackets",0]]', '[["hats",-1],["glasses",-1],["earrings",-1],["mouth",-1],["lhand",-1],["rhand",-1],["watches",-1],["braclets",-1]]', '[-1,-1]'),
+	('steam:1100001359ad80f', 'deneme', '3', '1885233650', '{"1":["masks",0],"2":["hair",0],"3":["torsos",0],"4":["legs",0],"5":["bags",0],"6":["shoes",1],"7":["neck",0],"8":["undershirts",0],"9":["vest",0],"10":["decals",0],"11":["jackets",0],"0":["face",0]}', '{"1":["glasses",-1],"2":["earrings",-1],"3":["mouth",-1],"4":["lhand",-1],"5":["rhand",-1],"6":["watches",-1],"7":["braclets",-1],"0":["hats",-1]}', '[["face",0],["masks",0],["hair",0],["torsos",0],["legs",0],["bags",0],["shoes",2],["neck",0],["undershirts",1],["vest",0],["decals",0],["jackets",11]]', '[["hats",-1],["glasses",-1],["earrings",-1],["mouth",-1],["lhand",-1],["rhand",-1],["watches",-1],["braclets",-1]]', '[1,1]'),
+	('steam:1100001359ad80f', 'deneme', '5', '1885233650', '{"1":["masks",0],"2":["hair",4],"3":["torsos",0],"4":["legs",0],"5":["bags",0],"6":["shoes",1],"7":["neck",0],"8":["undershirts",0],"9":["vest",0],"10":["decals",0],"11":["jackets",0],"0":["face",0]}', '{"1":["glasses",-1],"2":["earrings",-1],"3":["mouth",-1],"4":["lhand",-1],"5":["rhand",-1],"6":["watches",-1],"7":["braclets",-1],"0":["hats",3]}', '[["face",0],["masks",0],["hair",0],["torsos",0],["legs",0],["bags",0],["shoes",2],["neck",0],["undershirts",1],["vest",0],["decals",0],["jackets",11]]', '[["hats",0],["glasses",-1],["earrings",-1],["mouth",-1],["lhand",-1],["rhand",-1],["watches",-1],["braclets",-1]]', '[1,1]'),
+	('steam:1100001359ad80f', 'saa', '7', '1885233650', '{"1":["masks",0],"2":["hair",0],"3":["torsos",0],"4":["legs",0],"5":["bags",0],"6":["shoes",1],"7":["neck",0],"8":["undershirts",0],"9":["vest",0],"10":["decals",0],"11":["jackets",0],"0":["face",0]}', '{"1":["glasses",-1],"2":["earrings",-1],"3":["mouth",-1],"4":["lhand",-1],"5":["rhand",-1],"6":["watches",-1],"7":["braclets",-1],"0":["hats",4]}', '[["face",0],["masks",0],["hair",0],["torsos",0],["legs",0],["bags",0],["shoes",2],["neck",0],["undershirts",1],["vest",0],["decals",0],["jackets",11]]', '[["hats",0],["glasses",-1],["earrings",-1],["mouth",-1],["lhand",-1],["rhand",-1],["watches",-1],["braclets",-1]]', '[1,1]'),
+	('steam:1100001359ad80f', 'deneme', '8', '225514697', '{"1":["masks",0],"2":["hair",2],"3":["torsos",14],"4":["legs",13],"5":["bags",4],"6":["shoes",18],"7":["neck",0],"8":["undershirts",0],"9":["vest",8],"10":["decals",7],"11":["jackets",5],"0":["face",0]}', '{"1":["glasses",-1],"2":["earrings",-1],"3":["mouth",-1],"4":["lhand",-1],"5":["rhand",-1],"6":["watches",-1],"7":["braclets",-1],"0":["hats",-1]}', '[["face",3],["masks",0],["hair",0],["torsos",5],["legs",3],["bags",0],["shoes",15],["neck",0],["undershirts",0],["vest",0],["decals",1],["jackets",6]]', '[["hats",-1],["glasses",-1],["earrings",-1],["mouth",-1],["lhand",-1],["rhand",-1],["watches",-1],["braclets",-1]]', '[-1,-1]');
+/*!40000 ALTER TABLE `character_outfits` ENABLE KEYS */;
+
+-- tablo yapısı dökülüyor shx_rpv.playerstattoos
+CREATE TABLE IF NOT EXISTS `playerstattoos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) NOT NULL,
+  `tattoos` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- shx_rpv.playerstattoos: ~1 rows (yaklaşık) tablosu için veriler indiriliyor
+/*!40000 ALTER TABLE `playerstattoos` DISABLE KEYS */;
+INSERT INTO `playerstattoos` (`id`, `identifier`, `tattoos`) VALUES
+	(1, 'Char1steam:1100001359ad80f', '[[-1719270477,-1082736975],[1529191571,-1529401172]]');
+/*!40000 ALTER TABLE `playerstattoos` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ raid_clothes/server.lua
@@ -0,0 +1,291 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+local function checkExistenceClothes(cid, cb)
+    MySQL.Async.fetchAll("SELECT cid FROM character_current WHERE cid = @cid LIMIT 1;", {["cid"] = cid}, function(result)
+        local exists = result and result[1] and true or false
+        cb(exists)
+    end)
+end
+
+local function checkExistenceFace(cid, cb)
+    MySQL.Async.fetchAll("SELECT cid FROM character_face WHERE cid = @cid LIMIT 1;", {["cid"] = cid}, function(result)
+        local exists = result and result[1] and true or false
+        cb(exists)
+    end)
+end
+
+RegisterServerEvent("raid_clothes:insert_character_current")
+AddEventHandler("raid_clothes:insert_character_current",function(data)
+    if not data then return end
+    local src = source
+    local user = ESX.GetPlayerFromId(source)
+    local characterId = user.identifier
+    if not characterId then return end
+    checkExistenceClothes(characterId, function(exists)
+        local values = {
+            ["cid"] = characterId,
+            ["model"] = json.encode(data.model),
+            ["drawables"] = json.encode(data.drawables),
+            ["props"] = json.encode(data.props),
+            ["drawtextures"] = json.encode(data.drawtextures),
+            ["proptextures"] = json.encode(data.proptextures),
+        }
+
+        if not exists then
+            local cols = "cid, model, drawables, props, drawtextures, proptextures"
+            local vals = "@cid, @model, @drawables, @props, @drawtextures, @proptextures"
+
+            MySQL.Async.execute("INSERT INTO character_current ("..cols..") VALUES ("..vals..")", values, function()
+            end)
+            return
+        end
+
+        local set = "model = @model,drawables = @drawables,props = @props,drawtextures = @drawtextures,proptextures = @proptextures"
+        MySQL.Async.execute("UPDATE character_current SET "..set.." WHERE cid = @cid", values)
+    end)
+end)
+
+
+RegisterServerEvent("raid_clothes:insert_character_face")
+AddEventHandler("raid_clothes:insert_character_face",function(data)
+    if not data then return end
+    local src = source
+
+    local user = ESX.GetPlayerFromId(source)
+    local characterId = user.identifier
+
+    if not characterId then return end
+
+    checkExistenceFace(characterId, function(exists)
+        local values = {
+            ["cid"] = characterId,
+            ["hairColor"] = json.encode(data.hairColor),
+            ["headBlend"] = data.headBlend,
+            ["headOverlay"] = json.encode(data.headOverlay),
+            ["headStructure"] = json.encode(data.headStructure),
+        }
+        local set = "hairColor = @hairColor,headBlend = @headBlend, headOverlay = @headOverlay,headStructure = @headStructure"
+        MySQL.Async.execute("UPDATE character_face SET "..set.." WHERE cid = @cid", values )
+    end)
+end)
+
+RegisterServerEvent("raid_clothes:get_character_face")
+AddEventHandler("raid_clothes:get_character_face",function(pSrc)
+    local src = (not pSrc and source or pSrc)
+    local user = ESX.GetPlayerFromId(source)
+    local characterId = user.identifier
+
+    if not characterId then return end
+
+    MySQL.Async.fetchAll("SELECT cc.model, cf.hairColor, cf.headBlend, cf.headOverlay, cf.headStructure FROM character_face cf INNER JOIN character_current cc on cc.cid = cf.cid WHERE cf.cid = @cid", {['cid'] = characterId}, function(result)
+        if (result ~= nil and result[1] ~= nil) then
+            local temp_data = {
+                hairColor = json.decode(result[1].hairColor),
+                headBlend = json.decode(result[1].headBlend),
+                headOverlay = json.decode(result[1].headOverlay),
+                headStructure = json.decode(result[1].headStructure),
+            }
+            local model = tonumber(result[1].model)
+            if model == 1885233650 or model == -1667301416 then
+                TriggerClientEvent("raid_clothes:setpedfeatures", src, temp_data)
+            end
+        end
+	end)
+end)
+
+RegisterServerEvent("raid_clothes:get_character_current")
+AddEventHandler("raid_clothes:get_character_current",function(pSrc)
+    local src = (not pSrc and source or pSrc)
+    local user = ESX.GetPlayerFromId(source)
+    local characterId = user.identifier
+
+    if not characterId then return end
+
+    MySQL.Async.fetchAll("SELECT * FROM character_current WHERE cid = @cid", {['cid'] = characterId}, function(result)
+        local temp_data = {
+            model = result[1].model,
+            drawables = json.decode(result[1].drawables),
+            props = json.decode(result[1].props),
+            drawtextures = json.decode(result[1].drawtextures),
+            proptextures = json.decode(result[1].proptextures),
+        }
+        TriggerClientEvent("raid_clothes:setclothes", src, temp_data,0)
+	end)
+end)
+
+RegisterServerEvent("raid_clothes:retrieve_tats")
+AddEventHandler("raid_clothes:retrieve_tats", function(pSrc)
+    local src = (not pSrc and source or pSrc)
+	local user = ESX.GetPlayerFromId(source)
+    local char = user.identifier
+	MySQL.Async.fetchAll("SELECT * FROM playersTattoos WHERE identifier = @identifier", {['identifier'] = user.identifier}, function(result)
+        if(#result == 1) then
+			TriggerClientEvent("raid_clothes:settattoos", src, json.decode(result[1].tattoos))
+		else
+			local tattooValue = "{}"
+			MySQL.Async.execute("INSERT INTO playersTattoos (identifier, tattoos) VALUES (@identifier, @tattoo)", {['identifier'] = user.identifier, ['tattoo'] = tattooValue})
+			TriggerClientEvent("raid_clothes:settattoos", src, {})
+		end
+	end)
+end)
+
+RegisterServerEvent("raid_clothes:set_tats")
+AddEventHandler("raid_clothes:set_tats", function(tattoosList)
+	local src = source
+	local user = ESX.GetPlayerFromId(source)
+    local char = user.identifier
+	MySQL.Async.execute("UPDATE playersTattoos SET tattoos = @tattoos WHERE identifier = @identifier", {['tattoos'] = json.encode(tattoosList), ['identifier'] = user.identifier})
+end)
+
+
+RegisterServerEvent("raid_clothes:get_outfit")
+AddEventHandler("raid_clothes:get_outfit",function(slot)
+    if not slot then return end
+    local src = source
+
+    local user = ESX.GetPlayerFromId(source)
+    local characterId = user.identifier
+
+    if not characterId then return end
+
+    MySQL.Async.fetchall("SELECT * FROM character_outfits WHERE cid = @cid and slot = @slot", {
+        ['cid'] = characterId,
+        ['slot'] = slot
+    }, function(result)
+        if result and result[1] then
+            if result[1].model == nil then
+                TriggerClientEvent("notification", src, "Can not use.",2)
+                return
+            end
+
+            local data = {
+                model = result[1].model,
+                drawables = json.decode(result[1].drawables),
+                props = json.decode(result[1].props),
+                drawtextures = json.decode(result[1].drawtextures),
+                proptextures = json.decode(result[1].proptextures),
+                hairColor = json.decode(result[1].hairColor)
+            }
+
+            TriggerClientEvent("raid_clothes:setclothes", src, data,0)
+
+            local values = {
+                ["cid"] = characterId,
+                ["model"] = data.model,
+                ["drawables"] = json.encode(data.drawables),
+                ["props"] = json.encode(data.props),
+                ["drawtextures"] = json.encode(data.drawtextures),
+                ["proptextures"] = json.encode(data.proptextures),
+            }
+
+            local set = "model = @model, drawables = @drawables, props = @props,drawtextures = @drawtextures,proptextures = @proptextures"
+            MySQL.Async.execute("UPDATE character_current SET "..set.." WHERE cid = @cid",values)
+        else
+            TriggerClientEvent("notification", src, "No outfit on slot " .. slot,2)
+            return
+        end
+	end)
+end)
+
+RegisterServerEvent("raid_clothes:set_outfit")
+AddEventHandler("raid_clothes:set_outfit",function(slot, name, data)
+    if not slot then return end
+    local src = source
+
+    local user = ESX.GetPlayerFromId(source)
+    local characterId = user.identifier
+
+    if not characterId then return end
+
+    MySQL.Async.fetchall("SELECT slot FROM character_outfits WHERE cid = @cid and slot = @slot", {
+        ['cid'] = characterId,
+        ['slot'] = slot
+    }, function(result)
+        if result and result[1] then
+            local values = {
+                ["cid"] = characterId,
+                ["slot"] = slot,
+                ["name"] = name,
+                ["model"] = json.encode(data.model),
+                ["drawables"] = json.encode(data.drawables),
+                ["props"] = json.encode(data.props),
+                ["drawtextures"] = json.encode(data.drawtextures),
+                ["proptextures"] = json.encode(data.proptextures),
+                ["hairColor"] = json.encode(data.hairColor),
+            }
+
+            local set = "model = @model,name = @name,drawables = @drawables,props = @props,drawtextures = @drawtextures,proptextures = @proptextures,hairColor = @hairColor"
+            MySQL.Async.execute("UPDATE character_outfits SET "..set.." WHERE cid = @cid and slot = @slot",values)
+        else
+            local cols = "cid, model, name, slot, drawables, props, drawtextures, proptextures, hairColor"
+            local vals = "@cid, @model, @name, @slot, @drawables, @props, @drawtextures, @proptextures, @hairColor"
+
+            local values = {
+                ["cid"] = characterId,
+                ["name"] = name,
+                ["slot"] = slot,
+                ["model"] = data.model,
+                ["drawables"] = json.encode(data.drawables),
+                ["props"] = json.encode(data.props),
+                ["drawtextures"] = json.encode(data.drawtextures),
+                ["proptextures"] = json.encode(data.proptextures),
+                ["hairColor"] = json.encode(data.hairColor)
+            }
+
+            MySQL.Async.execute("INSERT INTO character_outfits ("..cols..") VALUES ("..vals..")", values, function()
+                TriggerClientEvent("notification", src, name .. " stored in slot " .. slot,1)
+            end)
+        end
+	end)
+end)
+
+
+RegisterServerEvent("raid_clothes:remove_outfit")
+AddEventHandler("raid_clothes:remove_outfit",function(slot)
+
+    local src = source
+    local user = ESX.GetPlayerFromId(source)
+    local cid = user.identifier
+    local slot = slot
+
+    if not cid then return end
+
+    MySQL.Async.execute( "DELETE FROM character_outfits WHERE cid = @cid AND slot = @slot", { ['cid'] = cid,  ["slot"] = slot } )
+    TriggerClientEvent("notification", src,"Removed slot " .. slot .. ".",1)
+end)
+
+RegisterServerEvent("raid_clothes:list_outfits")
+AddEventHandler("raid_clothes:list_outfits",function()
+    local src = source
+    local user = ESX.GetPlayerFromId(source)
+    local cid = user.identifier
+    local slot = slot
+    local name = name
+
+    if not cid then return end
+
+    MySQL.Async.fetchall("SELECT slot, name FROM character_outfits WHERE cid = @cid", {['cid'] = cid}, function(skincheck)
+    	TriggerClientEvent("hotel:list",src, skincheck)
+	end)
+end)
+
+RegisterServerEvent("clothing:checkMoney")
+AddEventHandler("clothing:checkMoney", function(menu,askingPrice)
+    
+    local src = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+
+    if xPlayer.getMoney() >= 0 then
+        xPlayer.removeMoney(0)
+        TriggerClientEvent('notification', source, 'You paid $0' )
+  
+        TriggerClientEvent("raid_clothes:hasEnough",src,menu)
+    else
+
+        TriggerClientEvent('notification', source, 'You dont have enough money' )
+     
+    end
+end)
