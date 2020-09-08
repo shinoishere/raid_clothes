4dd07bd72e35ffa97e8c5fa873220161de85757a
itsh1no <49691112+itsh1no@users.noreply.github.com>
2020-09-08 04:23:22 +0300

    Update __resource.lua

--- raid_clothes/__resource.lua
+++ raid_clothes/__resource.lua
@@ -1,6 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 server_script('server.lua')
+server_script('@mysql-async/lib/MySQL.lua')
 client_script('client/clothingshop.lua')
 client_script('client/tattooshop.lua')
 client_script('client/healingspots.lua')
